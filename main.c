cd87afb6659e4b54f619dd1c5e8bab05b62e5b3c
Chril <crazypizzak2@gmail.com>
2017-05-22 22:00:17 -0700

    Added test files and began expirmenting

--- main.c
+++ main.c
@@ -3,6 +3,14 @@
 #include <stdlib.h>
 #include "sptg_file.h"
 
+float x_min = 150;
+float x_max = 150;
+float y_min = 150;
+float y_max = 150;
+
+void scrapeCords(char line[]);
+
+
 
 int main(int argc, char const *argv[]) {
     FILE *inputFile = fopen(argv[1], "r+");
@@ -12,20 +20,23 @@ int main(int argc, char const *argv[]) {
 
     if (inputFile != NULL){
         while ( fgets(line, 200, inputFile) != NULL){
-            printf("%s", line);
+            // printf("%s", line);
             switch (line[0]){
                 case 'G':
                     scrapeCords(line); //Dynamically adjust size of tower/maybe dynamically size skirt?
                     break;
 
-                case 'TOOLCHANGE':
+                case 'T':       // Tool change
+                    // moveToPosition();
+                    // changeMaterial();
+                    // drawTower();
                     break;
 
-                case '':
-
+                case ';':       //Get info about user config
+                    // scrapeConfig(line)
                     break;
 
-                default:
+                // default:
 
             } //switch
         } //while
@@ -33,6 +44,61 @@ int main(int argc, char const *argv[]) {
     else{
         perror("File failed to open\n");
     }
+    printf("Xmin: %f    |    Xmax: %f\n", x_min, x_max );
     return 0;
 }
 
+// tower based on the size of the melt zone of hot end?
+
+
+
+void scrapeCords(char line[]){
+    const char key[2] = " ";
+    char *feild = strtok(line, key);
+    float num = 0;
+    char* temppter = NULL;
+
+    while (feild != NULL){
+        //printf("%s\n", feild);
+        switch (feild[0]){
+            case 'X':
+                *feild++;
+                num = strtof(feild, NULL);
+                //printf("feild:%s    |     Num:%f\n", feild, num);
+                if (num > x_max) x_max = num;
+                else if (num < x_min) x_min = num;
+                // sleep(1);
+                break;
+            case 'Y':
+                //printf("Found a Y\n");
+                break;
+            case 'Z':
+                printf("found a z\n");
+                break;
+        } //switch
+        
+        feild = strtok(NULL, key);
+    } //while
+    
+}
+
+
+//G1 X70.761 Y74.864 E0.01429
+// G91                       ; Relative moves.
+// G1 X50 Y50 Z2 F1000
+// G1 E4 F500           ; Quicky extrude about the length of your melt zone.
+// G1 E-2 F1000       ; Do a quick retraction a little shorter than your melt zone. This is to avoid stringing.
+// G4 P2500             ; Wait 2.5 seconds so that filament can set.
+// G1 E-108 F 8000  ; Quickly pull filament out of the Y-splitter.
+// G90                       ; Abolute moves.
+
+// ; Load in next extruder
+// T0 ;  Select next material.
+// G91                       ; Relative moves.
+// G1 E105 F2000     ; Quickly load next material 95%.
+// G1 E5 F2000         ; Slowly load last 5%.
+// G1 Z-2 F500
+// G90                       ; Abolute moves.
+// T0
+// G1 F5760
+// G1 X70.851 Y74.786 E-0.04736
