17a57da47b378eb9fd1fa2e4f280f93ed92ad077
Chril <crazypizzak2@gmail.com>
2017-05-25 22:26:30 -0700

    Cleaning up code

--- main.c
+++ main.c
@@ -1,135 +1,129 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
-#include "sptg_file.h"
 
+#define LINELENGTH 200
+
+/******************************** Notes ****************************************
+ *  Only need to do full prime tower layer after a material switch.
+ *    Just do outline or infill(more work).
+ * 
+ * Sizing prime tower:
+ *  ask user for area (l/w)?
+ *  pull nozzle size from gcode
+ *  pull feed rate from gcode?
+ * 
+ ******************************************************************************/
+
+
+/**
+ * Easy way to keep track of relative position.
+ */
+typedef enum Position{
+    START       = 1,
+    NORMAL      = 2,
+    MATBEGIN    = 3,
+    MATEND      = 4,
+    FLUSH       = 5,
+    END         = 6
+} position;
+
+typedef enum bool{
+    false = 0,
+    true  = 1
+} bool;
+
+/**
+ * Singingly linked list
+ */
+typedef struct node {
+    struct node *next;
+    char   data[200];
+} node_t;
+
+void scrapecords(char *line);
+void parsecomment(char *line);
+void add_tail(node_t *head, char *val);
+void flushlist(FILE *outputFile, node_t *head);
+
+
+int     pos   = 0;       /*<Used to keep track of where we are in the gcode.*/
 float   x_min = 1000;
 float   x_max = 0;
 float   y_min = 1000;
 float   y_max = 0;
-uint8_t scrapeEN = 0;
-uint8_t pastStart = 0;
-uint8_t deferOutput = 0;
-uint8_t emptyDeffer = 0;
-
-void scrapeCords(char line[]);
-void parseComment(char line[]);
 
 
+void main(int argc, char const *argv[]){
+    if (argc < 1) exit(1);     // Make sure argument is supplied
 
+    FILE *inputFile, *outputFile;               /*<I/O file pointers.*/
+    char line[LINELENGTH];                      /*<Line buffer.*/
+   
+    node_t *head = NULL;
+    // head = malloc(sizeof(node_t));
+    // head->next = NULL;
+    // strcpy(head->data, "\0");
 
-int main(int argc, char const *argv[]) {
-    FILE *inputFile = fopen(argv[1], "r");
-    FILE *outputFile = fopen(".tmp", "w");
-    char line[200];
-    char templine[201];
-    char deferbuff[100][200];
-    uint8_t counter = 0;
-    uint8_t temp = 0;
-    uint16_t lineBuffer_size = 200;
+    inputFile  = fopen(argv[1], "r");
+    if (inputFile == NULL) exit(1);
+    outputFile = fopen(".tmp", "w");
 
-
-    if (inputFile != NULL){
-        while ( fgets(line, 200, inputFile) != NULL){
-            
-            switch (line[0]){
-                case 'G':
-                    if (line[1] == '1' && scrapeEN) scrapeCords(line);
-                    break;
-
-                case 'T':       // Tool change
-                    // moveToPosition();
-                    // changeMaterial();
-                    // drawTower();
-                    break;
-
-                case 'M':
-                    if ( (line[3] == '9') && (pastStart == 1) ){    
-                        // remove line by putting a ; in front
-                        strcpy(&templine[1], line);
-                        templine[0] = ';';
-                        strcpy(line, templine);
-                    }
-                    break;
-
-                case ';':       //Get info about user config
-                    parseComment(line);
-                    break;
-
-                // default:
-
-            } //switch
-            if (deferOutput){
-                sprintf(deferbuff[counter], line);
-                counter++;
-            }
-            else if ( emptyDeffer && (deferOutput == 0) ){
-                for(temp; temp <= counter; temp++){
-                    //printf("%d: %s\n", temp, deferbuff[temp]);
-                    fputs(deferbuff[temp], outputFile);
+    while( fgets(line, LINELENGTH, inputFile) != NULL ){
+        switch (line[0]){
+            case ';':           // Comment
+                parsecomment(line);
+                break;
+            case 'G':           // Movement
+                if (pos == NORMAL) scrapecords(line);  // Used for auto placement
+                break;
+            case 'M':           // Remove redundant waits for temp M109 by putting ';'
+                if ( (pos >= START) && (line[3] == '9') ){
+                    line[0] = ';';
                 }
 
-                emptyDeffer = 0;
-                counter = 0;
-                temp = 0;
-                fputs(line, outputFile);
-            }
+                break;
 
-            else fputs(line, outputFile); // Write any changes made to a line to output
+        } //switch
 
-        } //while
-    } //if
-    else{
-        perror("File failed to open\n");
-        return -1;
-    }
-    printf("Xmin: %f    |    Xmax: %f\n", x_min, x_max );
-    printf("Ymin: %f    |    Ymax: %f\n", y_min, y_max );
-    
+        if      (pos == MATBEGIN) add_tail(head, line);       // Defer nozzle switching gcode
+        else if (pos == FLUSH){
+            flushlist(outputFile, head); // Flush after printer moved to prime tower
+            pos = NORMAL;
+        }
+        else                      fputs(line, outputFile);      // Copy lines out otherwise
+    } //while
+
     fclose(inputFile);
     fclose(outputFile);
-    return 0;
+    exit(0);
 }
 
-// tower based on the size of the melt zone of hot end?
-
-
-void parseComment(char line[]){
-    char linecpy[200];
-    strcpy(linecpy, line);
-    if ( (strcmp(linecpy, ";START\n")) == 0){
-        pastStart = 1;
-    }
-    else if ( (strcmp(linecpy, ";STARTEND\n")) == 0){
-        scrapeEN = 1;
-    }
-    else if ( (strcmp(linecpy, ";END\n")) == 0){
-        scrapeEN = 0;
-    }
-    else if ( (strcmp(linecpy, ";EEND\n")) == 0){
-        deferOutput = 1;
-    }
-    else if ( (strcmp(linecpy, ";ESTARTEND\n")) == 0){
-        deferOutput = 0;
-    }
-    else if ( linecpy[1] == 'T') {
-        emptyDeffer = 1;
-    }
+/**
+ * @brief      Looks for predefined keywords.
+ *
+ * @param      line  The buffer from @inputFile
+ */
+void parsecomment(char *line){
+    if      ( strcmp(line, ";START\n")    == 0 )           pos = START;
+    else if ( strcmp(line, ";MATBEGIN\n") == 0 )           pos = MATBEGIN;
+    else if ( strcmp(line, ";MATEND\n") == 0 )             pos = MATEND;
+    else if ( (line[1] == 'T') && (pos == MATEND) )      pos = FLUSH;   // Only CURA
+    else if ( strcmp(line, ";END\n")      == 0 )           pos = END;
+    else if ( pos >= START && pos < END )                pos = NORMAL;   // Any comment after start will change state to normal
 }
 
 
-
-void scrapeCords(char line[]){
+void scrapecords(char *line){
     const char key[2] = " ";
     float num = 0;
     char* temppter = NULL;
     char linecpy[200];
-    strcpy(linecpy, line);
+    strcpy(linecpy, line);      // Strtok messes up strings
 
     char *feild = strtok(linecpy, key);
 
     while (feild != NULL){
-        //printf("%s\n", feild);
         switch (feild[0]){
             case 'X':
                 *feild++;
@@ -147,28 +141,45 @@ void scrapeCords(char line[]){
                 
                 break;
         } //switch
-        
         feild = strtok(NULL, key);
-    } //while    
+    } //while 
 } // scrapeCords
 
 
-//G1 X70.761 Y74.864 E0.01429
-// G91                       ; Relative moves.
-// G1 X50 Y50 Z2 F1000
-// G1 E4 F500           ; Quicky extrude about the length of your melt zone.
-// G1 E-2 F1000       ; Do a quick retraction a little shorter than your melt zone. This is to avoid stringing.
-// G4 P2500             ; Wait 2.5 seconds so that filament can set.
-// G1 E-108 F 8000  ; Quickly pull filament out of the Y-splitter.
-// G90                       ; Abolute moves.
+/**
+ * @brief      Adds a new node to the tail.
+ *
+ * @param      head  The head of the list.
+ * @param[in]  val   The value to store.
+ */
+void add_tail(node_t *head, char *val){
+    node_t *current = head;
+    if (head != NULL){
+    while (current->next != NULL){
+        current = current->next;
+    }
+    current->next = malloc(sizeof(node_t));
+    strcpy(current->next->data, val);
+    current->next->next = NULL;
+    }
+    else{
+        printf("making new head\n");
+        head = malloc(sizeof(node_t));
+        strcpy(head->data, val);
+        head->next = NULL;   
+    }
+}
 
-// ; Load in next extruder
-// T0 ;  Select next material.
-// G91                       ; Relative moves.
-// G1 E105 F2000     ; Quickly load next material 95%.
-// G1 E5 F2000         ; Slowly load last 5%.
-// G1 Z-2 F500
-// G90                       ; Abolute moves.
-// T0
-// G1 F5760
-// G1 X70.851 Y74.786 E-0.04736
+
+
+void flushlist(FILE *outputFile, node_t *head){
+    node_t *temp = head;
+    printf("in flush\n");
+    while (head->next != NULL){
+        printf("%s\n", head->data);
+        fputs(head->data, outputFile);
+        head = head->next;
+        free(temp);
+        temp = head;
+    }
+}
